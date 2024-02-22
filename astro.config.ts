9e26aa3ebd0d321c0fedc560c05f49ce18b23895
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-02-22 13:35:06 +0100

    Prepare for v1.5.0 of the bag

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import expressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc';
 
-import vtbot from 'astro-vtbot';
+import vtbot from 'astro-vtbot/integration/index.ts';
 
 // https://astro.build/config
 export default defineConfig({
