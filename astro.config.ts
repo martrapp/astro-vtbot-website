2cdd9fb6678b1c6ea3ae86537c83d3f87dc703f3
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-02-22 13:39:52 +0100

    Fix wrong import

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import expressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc';
 
-import vtbot from 'astro-vtbot/integration/index.ts';
+import vtbot from 'astro-vtbot';
 
 // https://astro.build/config
 export default defineConfig({
